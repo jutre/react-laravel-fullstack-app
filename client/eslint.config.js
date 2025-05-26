a8d02e13d52857b2e40bdf8284a6f577a36d39f6
ju tre <juris.treibahs@gmail.com>
2025-05-26 15:46:52 +0300

    added ESlint with some basic cofiguration

---
+++ client/eslint.config.js
@@ -0,0 +1,52 @@
+import tseslint from 'typescript-eslint'
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    // specify the formats on which to apply the rules below
+    files: ['**/*.{ts,tsx}'],
+    // use predefined configs in installed eslint plugins
+    extends: [
+      // js
+      js.configs.recommended,
+      // ts
+      ...tseslint.configs.recommended,
+      // react
+      react.configs.flat.recommended,
+      // import
+      importPlugin.flatConfigs.recommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    // specify used plugins
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+    settings: {
+      // for eslint-plugin-react to auto detect react version
+      react: {
+        version: 'detect'
+      },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json'
+        }
+      }
+    },
+    rules: {
+      // set of custom rules
+      'no-console': 'warn',
+      'react/button-has-type': 'error',
+      'react/react-in-jsx-scope': ['off'],
+    }
+  }
+)
