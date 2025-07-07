30d05c41b3857daf268ccd7ea22ad799a1045bd4
ju tre <juris.treibahs@gmail.com>
2025-07-07 13:10:44 +0300

    comment improvement

--- client/src/types/Book.ts
+++ client/src/types/Book.ts
@@ -2,15 +2,15 @@ export type Book = {
   id: number,
   title: string,
   author: string,
+  //'preface' property can also have null value in case receiving such value over REST API from backend which originates from null value of
+  //corresponding column in database
   preface: string | null,
-  //property name is in snake case as REST api sends JSON object with properties which are equel to RDMS table column names which are in
-  //snake case ('added_to_favorites' specifically does not came from table column name but in general JSON can contain properties conforming
-  //to column names)
+  //property name is in snake case as REST API sends JSON object with property name same as database table column name which are in snake
+  //case
   added_to_favorites?: boolean
 }
 
 export type NewBook = Omit<Book, 'id'>
 
-//favorite book cache in RTK Query cache will contain only 'id' field from book, it is needed to maintain only list of book ids to store
-//favorite books list
+//favorite book will contain only list of book identifiers of books that are added to favorite list
 export type FavoriteBook = Pick<Book, 'id'>
