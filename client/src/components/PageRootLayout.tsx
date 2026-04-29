c47d153df25faacefaea31fa5e5382933a15654b
ju tre <juris.treibahs@gmail.com>
2026-04-29 17:26:00 +0300

    refactoring - moving URL routing, user info fetching indicator code to separate file

--- client/src/components/PageRootLayout.tsx
+++ client/src/components/PageRootLayout.tsx
@@ -1,87 +1,25 @@
-import { routes } from "../config";
-import { PageHeader } from "./page_header/PageHeader";
-import { BooksListTypeMenu } from "./BooksListTypeMenu";
-import { FilteredBooksListInitializer } from "./books_list/FilteredBooksListInitializer";
-import { AllBooksList } from "./books_list/AllBooksList";
-import { FavoriteBookList } from "./books_list/FavoriteBookList";
-import { BookEditing } from "./BookEditing";
-import { DemoDataReset } from "./DemoDataReset";
+import { PageHeader } from "./page_header/PageHeader"
+import { BooksListTypeMenu } from "./BooksListTypeMenu"
 import {
   BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import { AuthenticatedRoute } from "./AuthenticatedRoute";
-import { useAppDispatch, useAppSelector } from '../store/reduxHooks';
-import { UserInfoAndLogoutControls } from "./UserInfoAndLogoutControls";
-import { BookCreating } from "./BookCreating";
-import { PageNotFound } from "./PageNotFound";
-import { apiSlice, selectIsUserLoggenIn } from "../features/api/apiSlice";
-import { ResourcesPreloader } from './ResourcesPreloader';
-import { BooksListLoadingSketeton } from "./books_list/BooksListLoadingSketeton";
+} from "react-router-dom"
+import { RoutesSwitch } from "./RoutesSwitch"
+import { useAppSelector } from '../store/reduxHooks'
+import { UserInfoAndLogoutControls } from "./UserInfoAndLogoutControls"
+import { selectIsUserLoggenIn } from "../features/api/apiSlice"
 
 /**
- * returns markup that creates layout structure (three columns beginning with larget tablet devices, one column on smaller tablet devices,
- * phones) and outputs menu, header and content parts. If user is authenticated displays all sections, if user is not logged in the
- * menu, search bar in header is hidden, login form is shown in content section
+ * Returns markup that creates layout structure (one column on smaller screems, three columns beginning with larger tablets)
+ * and outputs menu, header and content parts; hiding some on them if user is not authenticated.
  * @returns
  */
 export function PageRootLayout () {
-  const dispatch = useAppDispatch();
 
   //getting loading status and final result on whether user is logged in. If any other error besides "HTTP 401 Unauthenticated"
   //occurs like "HTTP 500 Internal Server Error" the login form will be displayed; if same error will still be present the login form will
   //display it when submitting it
-  const { isLoading: userDataInitialLoadStatus } = apiSlice.endpoints.getCurrentLoggedInUser.useQueryState()
   const isUserLoggenIn = useAppSelector(selectIsUserLoggenIn)
 
-
-  let mainContent: React.ReactNode;
-
-  // we don't know yet whether user is authenticated or not, display skeleton while waiting response from REST API with information whether
-  // HTTP session of authenticated user exists
-  if (userDataInitialLoadStatus === true) {
-    mainContent = <BooksListLoadingSketeton />
-
-  // now we know user whether user is authenticated or not
-  } else {
-
-    if (isUserLoggenIn) {
-      //launch fetching literary genres list to be already loaded when user opens book creation or edit form
-      dispatch(apiSlice.endpoints.getLiteraryGenres.initiate())
-    }
-
-    //display element matching to route if user is authenticated or login form if not authenticated
-    mainContent = (
-      <Routes>
-        <Route element={<AuthenticatedRoute isAuthenticated={isUserLoggenIn} />}>
-          <Route path={routes.bookListPath} element={<AllBooksList />} />
-
-          <Route path={routes.filteredBookListPath} element={<FilteredBooksListInitializer />} />
-
-          <Route path={routes.favoriteBooksListPath} element={<FavoriteBookList />} />
-
-          <Route path={routes.bookEditPath}
-            element={
-              <ResourcesPreloader>
-                <BookEditing />
-              </ResourcesPreloader>
-            } />
-
-          <Route path={routes.createBookPath}
-            element={
-              <ResourcesPreloader>
-                <BookCreating />
-              </ResourcesPreloader>
-            } />
-
-          <Route path={routes.demoDataResetPath} element={<DemoDataReset />} />
-        </Route>
-
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    )
-  }
   
   return (
     <div className="bg-[#eeeeee] flex min-h-screen">
@@ -104,17 +42,17 @@ export function PageRootLayout () {
         <div className="grow lg:grow-0 lg:shrink-0 lg:basis-[840px] xl:basis-[950px] flex flex-col relative">
           <PageHeader isUserLoggenIn={isUserLoggenIn}/>
 
-          {/*logout controls at top right of page*/}
-          {isUserLoggenIn === true &&
+          {/*logout controls at top right of page*/
+            isUserLoggenIn === true &&
             <div className="absolute top-[10px] right-[15px] sm:right-[30px] md:right-[15px] z-[1100]">
-              <UserInfoAndLogoutControls/>
+              <UserInfoAndLogoutControls />
             </div>
           }
 
 
-          {/*content - book lists or login form*/}
+          {/*content - components conforming to page URL or login form*/}
           <div className="bg-white relative pt-[30px] px-[15px] pb-[65px] xl:pb-[30px] sm:px-[30px] grow">
-            {mainContent}
+            <RoutesSwitch/>
           </div>
 
 
---
+++ client/src/components/RoutesSwitch.tsx
@@ -0,0 +1,79 @@
+import { routes } from "../config";
+import { FilteredBooksListInitializer } from "./books_list/FilteredBooksListInitializer";
+import { AllBooksList } from "./books_list/AllBooksList";
+import { FavoriteBookList } from "./books_list/FavoriteBookList";
+import { BookEditing } from "./BookEditing";
+import { DemoDataReset } from "./DemoDataReset";
+import {
+    Routes,
+    Route
+} from "react-router-dom";
+import { AuthenticatedRoute } from "./AuthenticatedRoute";
+import { useAppDispatch, useAppSelector } from '../store/reduxHooks';
+import { BookCreating } from "./BookCreating";
+import { PageNotFound } from "./PageNotFound";
+import { apiSlice, selectIsUserLoggenIn } from "../features/api/apiSlice";
+import { ResourcesPreloader } from './ResourcesPreloader';
+import { BooksListLoadingSketeton } from "./books_list/BooksListLoadingSketeton";
+
+/**
+ * Performs component routing using react-router API and some other things:
+ * 
+ * 1) returns component that matches defined route if user is authenticated or returns login form if user is not authenticated.
+ * 2) while getting user authentification status displays loading skeleton
+ * 3) initiates literary genres list loading as soon it is known that user is authenticated
+ * 
+ * @returns 
+ */
+export function RoutesSwitch() {
+    const dispatch = useAppDispatch();
+
+    //getting loading status and final result on whether user is logged in. If any other error besides "HTTP 401 Unauthenticated"
+    //occurs like "HTTP 500 Internal Server Error" the login form will be displayed; if same error will still be present the login form will
+    //display it when submitting it
+    const { isLoading: userDataInitialLoadStatus } = apiSlice.endpoints.getCurrentLoggedInUser.useQueryState()
+    const isUserLoggenIn = useAppSelector(selectIsUserLoggenIn)
+
+    if (userDataInitialLoadStatus === true) {
+        return <BooksListLoadingSketeton />
+
+        // now we know user whether user is authenticated or not
+    } else {
+
+        if (isUserLoggenIn) {
+            //launch fetching literary genres list to be already loaded when user opens book creation or edit form
+            dispatch(apiSlice.endpoints.getLiteraryGenres.initiate())
+        }
+
+        //display element matching to route if user is authenticated or login form if not authenticated
+        return (
+            <Routes>
+                <Route element={<AuthenticatedRoute isAuthenticated={isUserLoggenIn} />}>
+                    <Route path={routes.bookListPath} element={<AllBooksList />} />
+
+                    <Route path={routes.filteredBookListPath} element={<FilteredBooksListInitializer />} />
+
+                    <Route path={routes.favoriteBooksListPath} element={<FavoriteBookList />} />
+
+                    <Route path={routes.bookEditPath}
+                        element={
+                            <ResourcesPreloader>
+                                <BookEditing />
+                            </ResourcesPreloader>
+                        } />
+
+                    <Route path={routes.createBookPath}
+                        element={
+                            <ResourcesPreloader>
+                                <BookCreating />
+                            </ResourcesPreloader>
+                        } />
+
+                    <Route path={routes.demoDataResetPath} element={<DemoDataReset />} />
+                </Route>
+
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        )
+    }
+}
